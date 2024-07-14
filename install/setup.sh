626d091b53d5b8184c622737480208c3cc5cbd96
aarzaak <139879547+aarzaak@users.noreply.github.com>
2024-07-14 20:26:45 +0200

    setup.sh will now remove unused kernels (v7, v7l, v8)

--- install/setup.sh
+++ install/setup.sh
@@ -1,8 +1,9 @@
 cd $HOME
 
-sudo apt-get update
-sudo apt-get upgrade -y
-sudo apt-get install git -y
+sudo apt remove linux-*v8 linux-*v7l linux-*v7 -y
+sudo apt update
+sudo apt upgrade -y
+sudo apt install git -y
 
 git clone https://github.com/aarzaak/bthidhub
 cd $HOME/bthidhub/install/on_rpi
