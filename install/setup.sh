51923b80354c995ba94f09dcc52e18308b6fa657
aarzaak <139879547+aarzaak@users.noreply.github.com>
2024-07-18 10:54:26 +0200

    fixed a typo

--- install/setup.sh
+++ install/setup.sh
@@ -6,7 +6,7 @@ sudo apt update
 sudo apt upgrade -y
 
 sudo apt-key export 90FDDD2E | sudo gpg --dearmour -o /etc/apt/trusted.gpg.d/raspberrypi-debian-armhf.gpg
-sudo mv /etc/apt/sources.list /etc/apt/sources.list/bak
+sudo mv /etc/apt/sources.list /etc/apt/sources.list.bak
 sudo mv /etc/apt/sources.list.d/raspi.list /etc/apt/sources.list.d/raspi.list.bak
 sudo cp bthidhub/install/sources.list /etc/apt/sources.list
 sudo cp bthidhub/install/raspi.list /etc/apt/sources.list.d/raspi.list
