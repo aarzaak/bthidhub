faa2b715ca913fb8c06bdd4deaabac375e98f3f5
aarzaak <139879547+aarzaak@users.noreply.github.com>
2024-07-18 11:50:18 +0200

    removed fix for apt warning

--- install/setup.sh
+++ install/setup.sh
@@ -3,11 +3,5 @@
 sudo apt update
 sudo apt upgrade -y
 
-sudo apt-key export 90FDDD2E | sudo gpg --dearmour -o /etc/apt/trusted.gpg.d/raspberrypi-debian-armhf.gpg
-sudo mv /etc/apt/sources.list /etc/apt/sources.list.bak
-sudo mv /etc/apt/sources.list.d/raspi.list /etc/apt/sources.list.d/raspi.list.bak
-sudo cp bthidhub/install/sources.list /etc/apt/sources.list
-sudo cp bthidhub/install/raspi.list /etc/apt/sources.list.d/raspi.list
-
 cd on_rpi
 ./on_pi_setup.sh
