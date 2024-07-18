7587358714126562c8319d4f3df977ca989298e7
aarzaak <139879547+aarzaak@users.noreply.github.com>
2024-07-18 10:14:16 +0200

    removed installation of git, user is now responsible for that

--- install/setup.sh
+++ install/setup.sh
@@ -7,8 +7,6 @@ sudo apt remove linux-*v8 linux-*v7l linux-*v7 -y
 sudo apt update
 sudo apt upgrade -y
 
-sudo apt install git -y
-
 sudo apt-key export 90FDDD2E | sudo gpg --dearmour -o /etc/apt/trusted.gpg.d/raspberrypi-debian-armhf.gpg
 sudo mv /etc/apt/sources.list /etc/apt/sources.list/bak
 sudo mv /etc/apt/sources.list.d/raspi.list /etc/apt/sources.list.d/raspi.list.bak
