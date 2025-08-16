6a0294ba1132f2fb35d6bba97a390863da5237e2
aarzaak <139879547+aarzaak@users.noreply.github.com>
2025-08-16 17:29:48 +0200

    updated references to point to my repositories

--- install/on_rpi/on_pi_setup.sh
+++ install/on_rpi/on_pi_setup.sh
@@ -7,7 +7,7 @@ sudo apt-get install git libcairo2-dev libdbus-1-dev libgirepository1.0-dev libg
 sudo pip3 install -r $HOME/bthidhub/requirements.txt --break-system-packages
 
 cd $HOME/bthidhub/install/on_rpi
-git clone https://github.com/Dreamsorcerer/bluez.git --depth 1
+git clone https://github.com/aarzaak/bluez.git --depth 1
 cd $HOME/bthidhub/install/on_rpi/bluez
 autoreconf -fvi
 
